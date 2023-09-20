1aebcb3805057a113af55c8f3ab347df922d874f
Danylo <107757498+DanyloDiachenko@users.noreply.github.com>
2023-09-20 15:34:47 +0300

    product interface & content

---
+++ interfaces/pizza.interface.ts
@@ -0,0 +1,18 @@
+export interface IPizza {
+    _id: string;
+    rating: number;
+    img: string;
+    title: string;
+    size26: {
+        thin: number;
+        standard: number;
+    };
+    size30: {
+        thin: number;
+        standard: number;
+    };
+    size40: {
+        thin: number;
+        standard: number;
+    };
+}
